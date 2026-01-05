cfc2b8b8c5256be96a2075412fd5f7a7cfdb5124
Harris-PS <psharris2002@gmail.com>
2026-01-05 10:36:12 +0530

    first commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+function HomePage(){
+
+  return (
+    <div>
+      <div className="heading">
+        <p className="heading-text"></p>
+      </div>
+      <div className="blog-routes">
+        view blogs
+        create blogs
+      </div>
+    </div>
+  )
+}
