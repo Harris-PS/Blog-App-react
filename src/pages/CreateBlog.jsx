5b6966a3aceae69ba980b8c85f5fffbfbf120fe5
Harris-PS <psharris2002@gmail.com>
2026-01-06 16:51:21 +0530

    created the CreateBlog function to add new blog posts

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -1,8 +1,40 @@
+import { useState, useContext } from "react"
+import { BlogContext } from "../contexts/BlogContext"
+
 
 function CreateBlog() {
-  
+
+  const [title,setTitle] = useState("");
+  const [description,setDescription] = useState("");
+  const { posts, setPost } = useContext(BlogContext);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    
+    if (title.trim() !== "" && description.trim()!=="") {
+      const newPost = {
+      id: posts.length + 1,
+      title: title,
+      description: description
+      };
+      setPost([...posts, newPost])
+
+      setTitle("");
+      setDescription("");
+    }
+
+  };
+
   return (
-    <div>CreateBlog</div>
+    <div className="create-container">
+      <form onSubmit={handleSubmit}>
+        <label className="title-label">Enter Title :</label>
+        <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)}/>
+        <label className="description-label">Enter Description :</label>
+        <input type="text" value={description} onChange={(e)=>setDescription(e.target.value)}/>
+        <button className="form-btn">Submit</button>
+      </form>
+    </div>
   )
 }
 
