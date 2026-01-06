c7d2901263399b5dd682d3a788232679d6ab96d1
Harris-PS <psharris2002@gmail.com>
2026-01-06 18:28:27 +0530

    Fixed an error in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/Blog-App-react/">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
